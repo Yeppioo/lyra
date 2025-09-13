647f251bef3560d883a0c3e09085c99ce1c29e77
Yeppioo <yeppioo@qq.com>
2025-09-13 22:12:42 +0800

    ci: 更新生产环境 API 地址为本地服务
    - 将生产环境的网易云音乐 API 地址从远程服务器更改为本地服务
    - 添加了远程地址的注释，便于未来切换

--- config.ts
+++ config.ts
@@ -1,5 +1,6 @@
 export const config = {
-  ProductionNeteaseApiBase: 'https://neteasemusic.api.yeppioo.vip',
+  // ProductionNeteaseApiBase: 'https://neteasemusic.api.yeppioo.vip',
+  ProductionNeteaseApiBase: 'http://localhost:3000',
   DevNeteaseApiBase: 'http://localhost:3000',
   realIP: '',
 };
