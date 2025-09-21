b917ad988804110d9bc1b3a4a99145af35679e9a
Yeppioo <yeppioo@qq.com>
2025-09-21 15:56:13 +0800

    feat(config): 更新生产环境API基础URL和真实IP配置
    将ProductionNeteaseApiBase配置更新为新的生产环境API地址，同时设置realIP为指定IP地址116.25.146.177以确保API请求的正确路由。

--- config.ts
+++ config.ts
@@ -2,5 +2,5 @@ export const config = {
   ProductionNeteaseApiBase: 'https://neteasemusic.api.yeppioo.vip',
   // ProductionNeteaseApiBase: 'http://localhost:3000',
   DevNeteaseApiBase: 'http://localhost:3000',
-  realIP: '',
+  realIP: '116.25.146.177',
 };
